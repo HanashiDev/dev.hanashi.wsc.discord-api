3f859dcd13e04cce59e99421b075766108c8679f
Peter Lohse <peterlohse@yahoo.de>
2019-08-15 13:18:26 +0200

    RuntimeCache hinzugefügt

---
+++ files/lib/system/cache/runtime/DiscordBotRuntimeCache.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace wcf\system\cache\runtime;
+use wcf\data\discord\bot\DiscordBotList;
+
+/**
+ * @author	Peter Lohse <hanashi@hanashi.eu>
+ * @copyright	Hanashi
+ * @license	Freie Lizenz (https://hanashi.dev/freie-lizenz/)
+ * @package	WoltLabSuite\Core\System\Cache\Runtime
+ */
+class DiscordBotRuntimeCache extends AbstractRuntimeCache {
+    /**
+     * @inheritDoc
+     */
+    protected $listClassName = DiscordBotList::class;
+}
