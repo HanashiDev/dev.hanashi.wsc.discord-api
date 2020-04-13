fd29af142f4100f5e134f755884235aa2ed5f90b
Peter Lohse <hanashi@hanashi.eu>
2020-04-13 18:10:11 +0200

    Snowflake Decoder eingebaut

--- files/lib/system/discord/DiscordApi.class.php
+++ files/lib/system/discord/DiscordApi.class.php
@@ -1240,31 +1240,6 @@ class DiscordApi {
         return $this->execute($url);
     }
 
-    public function getUserFlagsArray($flag) {
-        $userFlags = [
-            1 => 'Discord Employee', // 1 << 0
-            2 => 'Discord Partner', // 1 << 1
-            4 => 'HypeSquad Events', // 1 << 2
-            8 => 'Bug Hunter Level 1', // 1 << 3
-            64 => 'House Bravery', // 1 << 6
-            128 => 'House Brilliance', // 1 << 7
-            256 => 'House Balance', // 1 << 8
-            512 => 'Early Supporter', // 1 << 9
-            1024 => 'Team User', // 1 << 10
-            4096 => 'System', // 1 << 12
-            16384 => 'Bug Hunter Level 2', // 1 << 14
-        ];
-        $flags = [];
-
-        foreach ($userFlags as $userFlag => $description) {
-            if (($flag & $userFlag) == $userFlag) {
-                $flags[] = $description;
-            }
-        }
-
-        return $flags;
-    }
-
     /////////////////////////////////////
     // User End
     /////////////////////////////////////
@@ -1527,6 +1502,60 @@ class DiscordApi {
 
     /////////////////////////////////////
     // OAuth2 End
+    /////////////////////////////////////
+
+    /////////////////////////////////////
+    // Decoder Start
+    /////////////////////////////////////
+
+    /**
+     * returns encoded user flag informations
+     * 
+     * @param   integer $flag       Benutzer Flag als Decimalwert
+     * @return array
+     */
+    public function getUserFlagsArray($flag) {
+        $userFlags = [
+            1 => 'Discord Employee', // 1 << 0
+            2 => 'Discord Partner', // 1 << 1
+            4 => 'HypeSquad Events', // 1 << 2
+            8 => 'Bug Hunter Level 1', // 1 << 3
+            64 => 'House Bravery', // 1 << 6
+            128 => 'House Brilliance', // 1 << 7
+            256 => 'House Balance', // 1 << 8
+            512 => 'Early Supporter', // 1 << 9
+            1024 => 'Team User', // 1 << 10
+            4096 => 'System', // 1 << 12
+            16384 => 'Bug Hunter Level 2', // 1 << 14
+        ];
+        $flags = [];
+
+        foreach ($userFlags as $userFlag => $description) {
+            if (($flag & $userFlag) == $userFlag) {
+                $flags[] = $description;
+            }
+        }
+
+        return $flags;
+    }
+
+    /**
+     * returns encoded snowflake informations
+     * 
+     * @param   integer $snowflake  Snowflake ID as decimal
+     * @return array
+     */
+    public function decodeSnowflake($snowflake) {
+        return [
+            'timestamp' => round((($snowflake >> 22) + 1420070400000) / 1000),
+            'internalWorkerID' => ($snowflake & 0x3E0000) >> 17,
+            'internalProcessID' => ($snowflake & 0x1F000) >> 12,
+            'increment' => $snowflake & 0xFFF
+        ];
+    }
+
+    /////////////////////////////////////
+    // Decoder End
     /////////////////////////////////////
 
     /**
