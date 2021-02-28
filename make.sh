bdf2c3e9119ac52a1799472caf1c4d7bedba2fca
Peter Lohse <peterlohse@yahoo.de>
2021-02-28 15:35:42 +0100

    fix make.sh

--- make.sh
+++ make.sh
@@ -9,7 +9,7 @@ do
 done
 
 rm -rf ${PACKAGE_NAME}.tar ${PACKAGE_NAME}.tar.gz
-7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates_filebase -x!acptemplates_wcf -x!files_filebase -x!files_wcf -x!templates_filebase -x!templates_wcf -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.phpcs.xml -x!.github
+7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.phpcs.xml -x!.github
 
 for i in "${PACKAGE_TYPES[@]}"
 do
