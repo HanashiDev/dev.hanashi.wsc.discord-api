5e313127bd60163d2e2ca8ec44fb407fbd5810c8
Peter Lohse <github@hsrvmail.net>
2021-06-29 20:48:06 +0200

    Build Informationen hinzugefügt

--- make.sh
+++ make.sh
@@ -2,6 +2,9 @@
 PACKAGE_NAME=dev.hanashi.wsc.discord-api
 PACKAGE_TYPES=(acptemplates files)
 
+rm -rf files/js/*
+tsc --build
+
 for i in "${PACKAGE_TYPES[@]}"
 do
     rm -rf ${i}.tar
@@ -9,7 +12,7 @@ do
 done
 
 rm -rf ${PACKAGE_NAME}.tar ${PACKAGE_NAME}.tar.gz
-7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.phpcs.xml -x!.github
+7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.phpcs.xml -x!.github -x!ts -x!node_modules -x!package-lock.json -x!package.json -x!tsconfig.json
 
 for i in "${PACKAGE_TYPES[@]}"
 do
