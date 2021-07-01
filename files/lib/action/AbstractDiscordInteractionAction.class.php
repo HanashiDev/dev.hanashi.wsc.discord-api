199f573ec16f02322853a59d11bee9e18ee009cc
Peter Lohse <github@hsrvmail.net>
2021-07-01 10:25:52 +0200

    Interaction API eingebaut

---
+++ files/lib/action/AbstractDiscordInteractionAction.class.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace wcf\action;
+
+use wcf\system\discord\DiscordApi;
+use wcf\system\exception\SystemException;
+use wcf\util\JSON;
+
+abstract class AbstractDiscordInteractionAction extends AbstractAction implements IDiscordInteractionAction
+{
+    /**
+     * @inheritDoc
+     */
+    public function execute()
+    {
+        parent::execute();
+
+        try {
+            $body = file_get_contents('php://input');
+            if (empty($body)) {
+                throw new \BadMethodCallException('body is empty');
+            }
+            $data = [];
+            try {
+                $data = JSON::decode($body, true);
+            } catch (SystemException $e) {
+                throw new \BadMethodCallException('body is not valid json');
+            }
+
+            $publicKey = $this->getPublicKey();
+            if (empty($publicKey)) {
+                throw new \UnexpectedValueException('public key is empty');
+            }
+
+            if (!DiscordApi::verifyRequest($publicKey, $body)) {
+                throw new \OutOfBoundsException('invalid request signature');
+            }
+
+            if (empty($data['type'])) {
+                throw new \BadMethodCallException('type is empty');
+            }
+
+            switch ($data['type']) {
+                case DiscordApi::DISCORD_PING: {
+                    $this->sendPong();
+                    break;
+                }
+                case DiscordApi::DISCORD_APPLICATION_COMMAND: {
+                    $this->handleApplicationCommand($data);
+                    break;
+                }
+                case DiscordApi::DISCORD_MESSAGE_COMPONENT: {
+                    $this->handleMessageCommand($data);
+                    break;
+                }
+                default: {
+                    throw new \BadMethodCallException('unknown component');
+                }
+            }
+        } catch (\BadMethodCallException $e) {
+            @header('HTTP/1.1 400 Bad Request');
+            echo $e->getMessage();
+        } catch (\OutOfBoundsException $e) {
+            @header('HTTP/1.1 401 Unauthorized');
+            echo $e->getMessage();
+        } catch (\UnexpectedValueException $e) {
+            @header('HTTP/1.1 501 Not Implemented');
+            echo $e->getMessage();
+        }
+
+        $this->executed();
+    }
+
+    private function sendPong() {
+        echo JSON::encode([
+            'type' => DiscordApi::DISCORD_PONG
+        ]);
+    }
+}
---
+++ files/lib/action/IDiscordInteractionAction.class.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace wcf\action;
+
+interface IDiscordInteractionAction
+{
+    /**
+     * public key vom Discord Bot zurückgeben
+     *
+     * @return string
+     */
+    public function getPublicKey();
+    
+    /**
+     * verarbeitet die von Discord gesendeten Daten für Slash Commands
+     *
+     * @param  array $data
+     * @return void
+     */
+    public function handleApplicationCommand(array $data);
+    
+    /**
+     * verarbeitet die von Discord gesendeten Daten für Components
+     *
+     * @param  array $data
+     * @return void
+     */
+    public function handleMessageCommand(array $data);
+}
